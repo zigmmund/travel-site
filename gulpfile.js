e691c2dbbe54559abc9e99731f67dbe3515d0eae
Roman Yosypenko <yosypenkoif@gmail.com>
2017-04-19 16:31:54 +0300

    Add browserSync package

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    cssimport = require('postcss-import');
+    cssimport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log('Success!')
@@ -28,12 +29,24 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html')
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
