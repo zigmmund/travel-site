b8a67bb1ac0f4e545814f2452a5e7c6092136a01
Roman Yosypenko <yosypenkoif@gmail.com>
2017-04-19 18:25:31 +0300

    Add error handling message

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -14,5 +14,9 @@ gulp.task('styles', function() {
   ];
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss(plugins))
+    on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end')
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
